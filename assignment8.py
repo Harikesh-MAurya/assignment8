309cfc079a2d8b60a609cc15ddad9cc40bfc415d
Harikesh-MAurya <113936189+Harikesh-MAurya@users.noreply.github.com>
2022-09-24 15:19:19 +0530

    Add files via upload

---
+++ assignment8.py
@@ -0,0 +1,79 @@
+# 1. Write a python script to print MySirG 5 times on the screen
+# 2. Write a python script to print first 10 natural numbers
+# 3. Write a python script to print first 10 natural numbers in reverse order
+# 4. Write a python script to print first 10 odd natural numbers
+# 5. Write a python script to print first 10 odd natural numbers in reverse order
+# 6. Write a python script to print first 10 even natural numbers
+# 7. Write a python script to print first 10 even natural numbers in reverse order
+# 8. Write a python script to print squares of first 10 natural numbers
+# 9. Write a python script to print cubes of first 10 natural numbers
+# 10. Write a python script to print first 10 multiples of 5
+
+# 1) ..........................................................
+# i=1
+# while i<=5:
+#     print("MySirG")
+#     i=i+1
+
+
+# 2) .........................................................
+# i=1
+# while i<=10:
+#     print(i)
+#     i=i+1
+
+
+# 3) ........................................................
+# i=10
+# while i>=1:
+#     print(i,end=" ")
+#     i=i-1
+
+
+# 4) ..........................................................
+# i=1
+# while i<=21:
+#     print(i,end=" ")
+#     i=i+2
+
+
+# 5) .........................................................
+# i=21
+# while i>=1:
+#     print(i,end=" ")
+#     i=i-2
+
+
+# 6) .........................................................
+# i=1
+# while i<=10:
+#     print(2*i,end=" ")
+#     i=i+1
+
+
+# 7) ..........................................................
+# i=10
+# while i>=1:
+#     print(2*i,end=" ")
+#     i=i-1
+
+
+# 8) ........................................................
+# i=1
+# while i<=10:
+#     print(pow(i,2),end=" ")
+#     i=i+1
+
+
+# 9) ................................................................
+# i=1
+# while i<=10:
+#     print(pow(i,3),end=" ")
+#     i=i+1
+
+
+# 10) ..............................................................
+i=1
+while i<=10:
+    print(5*i,end=" ")
+    i=i+1
